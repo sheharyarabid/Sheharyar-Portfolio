05f3ad5d38a8d284ec76ff1ec080d61e108f4121
sheharyarabid <iamsheharyarabid@gmail.com>
2025-11-21 00:57:29 +0500

    f

--- app/components/pillbuttons/pillbuttons.jsx
+++ app/components/pillbuttons/pillbuttons.jsx
@@ -13,18 +13,18 @@ const PillButtons = () => {
         LinkedIn
       </a>
       <a
-        href="https://drive.google.com/file/d/1aAc9s9xWyOkNB4S14npOuDwfqkc4HjCK/view?usp=sharing" // replace with your local asset path
-        download
-        className={styles.button}
-      >
-        Download Resume
-      </a>
-      <a
         href="https://github.com/sheharyarabid" // replace with your local asset path
         download
         className={styles.button}
       >
         Github
+      </a>
+      <a
+        href="https://drive.google.com/file/d/1aAc9s9xWyOkNB4S14npOuDwfqkc4HjCK/view?usp=sharing" // replace with your local asset path
+        download
+        className={styles.button}
+      >
+        Download Resume
       </a>
 
     </div>
